53c05c76db68abc8730a47a166699f5ccac0375a
jethinthomas <73383651+jethinthomas@users.noreply.github.com>
2022-09-16 12:17:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # print(df.head())
+# st.set_page_config(layout="wide")
+
 st.title("Analysis based on people's lifestyle and health dataset")
 st.write("Source: https://www.numbeo.com")
 
@@ -24,14 +26,15 @@ rankings = 10
 rankings = st.selectbox('Rankings to be disaplayed ?',(5,10,15,20,25,30),1)
 
 bg_select = "quality_of_life"
-bg_option = ('quality_of_life','purchasing_power_index','safety_index','health_care_index','cost_of_living_index','property_price_to_income_ratio','traffic_commute_time_index','pollution_index','climate_index')
+bg_option = df.columns[2:]
+# bg_option = ('quality_of_life','purchasing_power_index','safety_index','health_care_index','cost_of_living_index','property_price_to_income_ratio','traffic_commute_time_index','pollution_index','climate_index')
 bg_select = st.selectbox('Rankings to be disaplayed ?',bg_option,0)
 
-
+df = df.sort_values(by=[bg_select], ascending=False)
 y_label = ' '.join(bg_select.split('_')).title()
 val_count  = df[bg_select][:rankings]
 fig = plt.figure(figsize=(10,5))
-sns.barplot(df.iloc[val_count.index,1], val_count.values)
+sns.barplot(df.loc[val_count.index,"country"], val_count.values)
 plt.xticks(rotation=70)
 plt.tight_layout()
 plt.title(f"TOP {rankings} rankings According to {y_label}")
@@ -39,9 +42,10 @@ plt.xlabel('Country')
 plt.ylabel(y_label)
 st.pyplot(fig)
 
-val_count  = df[bg_select][-rankings:]
+df = df.sort_values(by=[bg_select], ascending=True)
+val_count  = df[bg_select][:rankings]
 fig = plt.figure(figsize=(10,5))
-sns.barplot(df.iloc[val_count.index,1], val_count.values)
+sns.barplot(df.loc[val_count.index,"country"], val_count.values)
 plt.xticks(rotation=70)
 plt.tight_layout()
 plt.title(f"BOTTOM {rankings} rankings According to {y_label}")
@@ -49,3 +53,9 @@ plt.xlabel('Country')
 plt.ylabel(y_label)
 st.pyplot(fig)
 
+df = df.sort_values(by=[bg_select], ascending=False)
+clist = df["country"].tolist()
+countries = st.selectbox("Select country", clist, 0)
+st.write(f"You selected: {countries}")
+country_index = df.loc[df['country'] == countries].index[0]
+st.line_chart(df.iloc[country_index, 2:])
